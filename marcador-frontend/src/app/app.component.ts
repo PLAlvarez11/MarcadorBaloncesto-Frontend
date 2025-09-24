c6dedc8f5d8449113ccc21d6bd4b4e931156873d
PLAlvarez11 <106171305+PLAlvarez11@users.noreply.github.com>
2025-09-23 22:32:55 -0600

    UX pt2

--- marcador-frontend/src/app/app.component.ts
+++ marcador-frontend/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { LoaderComponent } from './shared/loader/loader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, LoaderComponent],
   template: `
     <app-navbar></app-navbar>
+    <app-loader></app-loader>
     <div class="container my-4">
       <router-outlet></router-outlet>
     </div>
